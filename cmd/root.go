d54dbd852fe33d7db9349bbae64b2f55bf2ea993
Alexander Strelnikov <aristat37@gmail.com>
2020-07-22 16:15:14 +0300

    New fix v3

--- cmd/root.go
+++ cmd/root.go
@@ -20,7 +20,6 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
-	fmt.Println("blabla")
 	rootCmd.AddCommand(chan_examples.Cmd, file_examples.Cmd)
 	if err := rootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err.Error())
